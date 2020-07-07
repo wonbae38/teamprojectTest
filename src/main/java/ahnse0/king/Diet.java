c1b43365faa213c736d23ceb1a43819b61994487
장원종 <97dnjswhd@daum.net>
2020-07-07 17:38:28 +0900

    no message

---
+++ src/main/java/ahnse0/king/Diet.java
@@ -0,0 +1,8 @@
+package ahnse0.king;
+
+public class Diet {
+
+	public void diet() {
+		System.out.println("다이어트는 너무 힘들다");
+	}
+}
---
+++ src/main/java/ahnse0/king/MyKing.java
@@ -0,0 +1,10 @@
+package ahnse0.king;
+
+public class MyKing {
+	
+	public static void main(String[] args) {
+		System.out.println("내가왕이다");
+		Diet diet = new Diet();
+		diet.diet();
+	}
+}
